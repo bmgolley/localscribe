24b1bd941079217307beb84ebbdd50a1a1e73349
Brandon Golley <bmgolley@gmail.com>
2024-09-10 12:53:40 -0500

    Update.

--- tests/modify_abilities.py
+++ tests/modify_abilities.py
@@ -1,18 +1,18 @@
-from context import localscribe_enhanced as ls
+from context import localscribe_enhanced as lse
 
 
-roster = ls.get_roster()
-roster_json = ls.create_json(roster, separateAbilities=True)
+roster = lse.get_roster()
+roster_json = lse.create_json(roster, separateAbilities=True)
 
-add: ls.AbilityParameters = {
-    frozenset({'Astra Militarum'}): {
+add: lse.AbilityChanges = {
+    frozenset({frozenset({'Astra Militarum'})}): {
         'Born Soldiers': 'Each time an ASTRA MILITARUM unit from your army Remains Stationary, until the end of the turn, ranged weapons equipped by models in that unit have the [LETHAL HITS] ability.'
     }
 }
-replace: ls.AbilityParameters = {
-    frozenset({'Militarum Tempestus Command Squad'}): {
+replace: lse.AbilityChanges = {
+    frozenset({frozenset({'Militarum Tempestus Command Squad'})}): {
         'Tempestor Prime': 'While this unit contains a TEMPESTOR PRIME, ranged weapons equipped by models in this unit have the [SUSTAINED HITS 1] ability.'
     }
 }
-ls.modify_abilities(roster_json, add=add, replace=replace)
+lse.modify_abilities(roster_json, add=add, replace=replace)
 pass
